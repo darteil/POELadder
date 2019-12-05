f8dc66f0d34badffa89358f1fdffe6bb6d2638da
Romanov Yuri <darteil.base@gmail.com>
2019-12-05 14:56:44 +0700

    Add Card component

--- src/LabyrinthLadder/index.jsx
+++ src/LabyrinthLadder/index.jsx
@@ -1,6 +1,6 @@
 /* global fetch */
 import React, { useState, useEffect } from "react";
-import { Button, Spinner } from "@blueprintjs/core";
+import { Button, Spinner, Card, Elevation } from "@blueprintjs/core";
 import classNames from "classnames";
 import Header from "../Header";
 import Pagination from "../Pagination";
@@ -79,74 +79,76 @@ const LabyrinthLadder = () => {
   return (
     <>
       <Header>Labyrinth ladder</Header>
-      <div className={styles["table-container"]}>
-        <div className={styles["table-wrapper"]}>
-          <div className={styles.filters}>
-            <div>
-              <label
-                htmlFor="event-select"
-                className={classNames("bp3-label", styles.label)}
-              >
-                Event
-                <div className="bp3-select">
-                  <select
-                    id="event-select"
-                    value={filter.event}
-                    onChange={selectEvent}
-                  >
-                    {eventsList.map(event => (
-                      <option key={event.value} value={event.value}>
-                        {event.text}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </label>
-              <label
-                htmlFor="class-select"
-                className={classNames("bp3-label", styles.label)}
-              >
-                Labyrinth type
-                <div className="bp3-select">
-                  <select
-                    id="class-select"
-                    value={filter.labType}
-                    onChange={selectLabType}
-                  >
-                    {labTypes.map(type => (
-                      <option key={type.value} value={type.value}>
-                        {type.text}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </label>
-            </div>
-            <Button icon="refresh" onClick={refreshTable}>
-              Refresh
-            </Button>
-          </div>
-          <div
-            className={classNames(
-              styles.table,
-              payload.loaded ? "" : styles.loading
-            )}
-          >
-            {!payload.loaded && (
+      <Card elevation={Elevation.THREE}>
+        <div className={styles["table-container"]}>
+          <div className={styles["table-wrapper"]}>
+            <div className={styles.filters}>
               <div>
-                <Spinner intent="Primary" size={100} />
+                <label
+                  htmlFor="event-select"
+                  className={classNames("bp3-label", styles.label)}
+                >
+                  Event
+                  <div className="bp3-select">
+                    <select
+                      id="event-select"
+                      value={filter.event}
+                      onChange={selectEvent}
+                    >
+                      {eventsList.map(event => (
+                        <option key={event.value} value={event.value}>
+                          {event.text}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </label>
+                <label
+                  htmlFor="class-select"
+                  className={classNames("bp3-label", styles.label)}
+                >
+                  Labyrinth type
+                  <div className="bp3-select">
+                    <select
+                      id="class-select"
+                      value={filter.labType}
+                      onChange={selectLabType}
+                    >
+                      {labTypes.map(type => (
+                        <option key={type.value} value={type.value}>
+                          {type.text}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </label>
               </div>
-            )}
-            {payload.loaded && <Table data={payload.data} />}
+              <Button icon="refresh" onClick={refreshTable}>
+                Refresh
+              </Button>
+            </div>
+            <div
+              className={classNames(
+                styles.table,
+                payload.loaded ? "" : styles.loading
+              )}
+            >
+              {!payload.loaded && (
+                <div>
+                  <Spinner intent="Primary" size={100} />
+                </div>
+              )}
+              {payload.loaded && <Table data={payload.data} />}
+            </div>
+            <Pagination
+              margin={2}
+              page={filter.page}
+              count={payload.records}
+              onPageChange={changePage}
+            />
           </div>
-          <Pagination
-            margin={2}
-            page={filter.page}
-            count={payload.records}
-            onPageChange={changePage}
-          />
         </div>
-      </div>
+      </Card>
     </>
   );
 };
