95c3f114f5c7bdddbdf7d4467000f20fbd5ec704
Romanov Yuri <darteil.base@gmail.com>
2019-12-06 16:19:22 +0700

    fix route

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,17 +1,17 @@
-import React, { Fragment } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Ladder from "../Ladder";
-import LabyrinthLadder from "../LabyrinthLadder";
-import Menu from "../Menu";
-import ErrorBoundary from "../ErrorBoundary";
-import NotFound from "../NotFound";
+import Ladder from '../Ladder';
+import LabyrinthLadder from '../LabyrinthLadder';
+import Menu from '../Menu';
+import ErrorBoundary from '../ErrorBoundary';
+import NotFound from '../NotFound';
 /* eslint-disable */
-import "!style-loader!css-loader!normalize.css/normalize.css";
-import "!style-loader!css-loader!@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "!style-loader!css-loader!@blueprintjs/core/lib/css/blueprint.css";
+import '!style-loader!css-loader!normalize.css/normalize.css';
+import '!style-loader!css-loader!@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '!style-loader!css-loader!@blueprintjs/core/lib/css/blueprint.css';
 /* eslint-enable */
-import styles from "./styles.css";
+import styles from './styles.css';
 
 const App = () => (
   <div className="bp3-dark">
@@ -21,9 +21,8 @@ const App = () => (
         <ErrorBoundary>
           <div className={styles.wrap}>
             <Switch>
-              <Route path="/" component={Ladder} />
               <Route path="/lab-ladder" component={LabyrinthLadder} />
-              <Route path="*" component={NotFound} />
+              <Route path="/" component={Ladder} />
             </Switch>
           </div>
         </ErrorBoundary>
