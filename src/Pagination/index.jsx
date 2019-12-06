027fd050a84525573736b7d359569856cd50278b
Romanov Yuri <darteil.base@gmail.com>
2019-12-06 16:19:33 +0700

    refactoring

--- src/Pagination/index.jsx
+++ src/Pagination/index.jsx
@@ -1,121 +1,127 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import styles from './styles.css';
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import styles from "./styles.css";
 
-export default class Pagination extends Component {
-  constructor(props) {
-    super(props);
+const Pagination = props => {
+  const [startPage, setStartPage] = useState(1);
+  const [endPage, setEndPage] = useState(3);
 
-    this.state = {
-      startPage: 1,
-      endPage: 3,
-      count: 10,
-      page: 1
-    };
+  useEffect(() => {
+    setStartPage(props.page > props.margin + 2 ? props.page - props.margin : 1);
+    setEndPage(
+      props.page + (props.margin + 2) > props.count
+        ? props.count
+        : props.page + props.margin
+    );
+  });
 
-    this.onPageChange = this.onPageChange.bind(this);
-    this.goFirstPage = this.goFirstPage.bind(this);
-    this.goLastPage = this.goLastPage.bind(this);
-    this.goPrevPage = this.goPrevPage.bind(this);
-    this.goNextPage = this.goNextPage.bind(this);
-  }
+  const onPageChange = event => {
+    const index = Array.prototype.indexOf.call(
+      event.target.parentNode.children,
+      event.target
+    );
+    props.onPageChange(index + startPage);
+  };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (prevState.count === nextProps.count && prevState.page === nextProps.page) return null;
+  const goFirstPage = () => {
+    props.onPageChange(1);
+  };
 
-    const { page, margin, count } = nextProps;
-    const startPage = page > (margin + 2) ? page - margin : 1;
-    const endPage = page + (margin + 2) > count ? count : page + margin;
-    return {
-      startPage, endPage, count, page
-    };
-  }
+  const goLastPage = () => {
+    props.onPageChange(props.count);
+  };
 
-  onPageChange(event) {
-    const index = Array.prototype.indexOf.call(event.target.parentNode.children, event.target);
-    this.props.onPageChange(index + this.state.startPage);
-  }
+  const goPrevPage = () => {
+    props.onPageChange(props.page - 1);
+  };
 
-  goFirstPage() {
-    this.props.onPageChange(1);
-  }
+  const goNextPage = () => {
+    props.onPageChange(props.page + 1);
+  };
 
-  goLastPage() {
-    this.props.onPageChange(this.state.count);
-  }
+  const pages = [];
 
-  goPrevPage() {
-    this.props.onPageChange(this.props.page - 1);
-  }
-
-  goNextPage() {
-    this.props.onPageChange(this.props.page + 1);
-  }
-
-  render() {
-    if (this.state.count === 0 || this.state.count <= 15) return null;
-
-    const { startPage, endPage, count } = this.state;
-    const { page, margin } = this.props;
-    const pages = [];
-
-    const firstPage = page - margin > 2 ? (
-      <button type="button" className="bp3-button" onClick={this.goFirstPage}>1</button>
+  const firstPage =
+    props.page - props.margin > 2 ? (
+      <button type="button" className="bp3-button" onClick={goFirstPage}>
+        1
+      </button>
     ) : null;
 
-
-    const lastPage = (page + margin) + 1 < count ? (
-      <button type="button" className="bp3-button" onClick={this.goLastPage}>{count}</button>
+  const lastPage =
+    props.page + props.margin + 1 < props.count ? (
+      <button type="button" className="bp3-button" onClick={goLastPage}>
+        {props.count}
+      </button>
     ) : null;
 
-    const prevPage = page === 1 ? null : (
-      <button type="button" className="bp3-button bp3-icon-chevron-left" onClick={this.goPrevPage} />
+  const prevPage =
+    props.page === 1 ? null : (
+      <button
+        type="button"
+        className="bp3-button bp3-icon-chevron-left"
+        onClick={goPrevPage}
+      />
     );
 
-    const nextPage = page === count ? null : (
-      <button type="button" className="bp3-button bp3-icon-chevron-right" onClick={this.goNextPage} />
+  const nextPage =
+    props.page === props.count ? null : (
+      <button
+        type="button"
+        className="bp3-button bp3-icon-chevron-right"
+        onClick={goNextPage}
+      />
     );
 
-    const prevSpreadBlock = page - margin > 2 ? (
-      <button type="button" className="bp3-button bp3-disabled">...</button>
+  const prevSpreadBlock =
+    props.page - props.margin > 2 ? (
+      <button type="button" className="bp3-button bp3-disabled">
+        ...
+      </button>
     ) : null;
 
-    const lastSpreadBlock = (page + margin) + 1 < count ? (
-      <button type="button" className="bp3-button bp3-disabled">...</button>
+  const lastSpreadBlock =
+    props.page + props.margin + 1 < props.count ? (
+      <button type="button" className="bp3-button bp3-disabled">
+        ...
+      </button>
     ) : null;
 
-    for (let i = startPage; i <= endPage; i += 1) {
-      pages.push(
-        <button
-          key={i}
-          type="button"
-          className={classNames('bp3-button', (this.props.page === i) ? 'bp3-active' : '')}
-          onClick={this.onPageChange}
-        >
-          {i}
-        </button>
-      );
-    }
-
-    return (
-      <div className={styles.pagination}>
-        {prevPage}
-        {firstPage}
-        {prevSpreadBlock}
-        <ul className={styles['pages-list']}>
-          {pages}
-        </ul>
-        {lastSpreadBlock}
-        {lastPage}
-        {nextPage}
-      </div>
+  for (let i = startPage; i <= endPage; i += 1) {
+    pages.push(
+      <button
+        key={i}
+        type="button"
+        className={classNames(
+          "bp3-button",
+          props.page === i ? "bp3-active" : ""
+        )}
+        onClick={onPageChange}
+      >
+        {i}
+      </button>
     );
   }
-}
+
+  return (
+    <div className={styles.pagination}>
+      {prevPage}
+      {firstPage}
+      {prevSpreadBlock}
+      <ul className={styles["pages-list"]}>{pages}</ul>
+      {lastSpreadBlock}
+      {lastPage}
+      {nextPage}
+    </div>
+  );
+};
 
 Pagination.propTypes = {
+  count: PropTypes.number.isRequired,
   page: PropTypes.number.isRequired,
   margin: PropTypes.number.isRequired,
   onPageChange: PropTypes.func.isRequired
 };
+
+export default Pagination;
