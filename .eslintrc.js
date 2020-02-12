fbbc05a1726a7146c08d5e712f76390f1d0e2420
Romanov Yuri <darteil.base@gmail.com>
2020-02-12 13:26:35 +0700

    update eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'prettier/prettier': 'error'
+  },
+  plugins: ['react', 'prettier', 'jsx-a11y']
+};
